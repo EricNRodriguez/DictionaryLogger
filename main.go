8d9ac6af49efe37b6f2bc8e7164e6ab28a152aa2
EricNRodriguez <rodriguez.eric.n@gmail.com>
2020-12-20 22:26:39 +1100

    basics, table not found issue on search

---
+++ main.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"database/sql"
+	// "encoding/json"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	// "net/http"
+	"os"
+	"strings"
+	"bytes"
+)
+
+// config
+const (
+	DIR = ".dictionarylogger"
+	DB  = "dictionarylogger.db"
+)
+
+// permissions
+const (
+	PERMISSIONS = 0755 // rwx, only owner can write and execute
+)
+
+// commands
+const (
+	SEARCH = "search"
+	LIST   = "list"
+	HELP   = "help"
+)
+
+// flags
+const (
+	ONELINE = "--oneline"
+)
+
+type SearchResult struct {
+	Meanings []struct {
+		PartOfSpeech string `json:"partOfSpeech"`
+		Definitions  []struct {
+			Definition string `json:"definition"`
+			Example    string `json:"example"`
+		} `json:"definitions"`
+	} `json: "meanings"`
+}
+
+func (sr SearchResult) String() string {
+	
+	var buffer bytes.Buffer
+	
+
+	for i := 0; i < len(sr.Meanings); i++ {
+		buffer.WriteString(fmt.Sprintf("%s\n", sr.Meanings[i].PartOfSpeech))
+		for j := 0; j < len(sr.Meanings[i].Definitions); j++ {
+			buffer.WriteString(fmt.Sprintf("\tDefinition: \n \t\t\t\t %s\n", sr.Meanings[i].Definitions[j].Definition))
+			buffer.WriteString(fmt.Sprintf("\tExample: \n \t\t\t\t %s\n\n", sr.Meanings[i].Definitions[j].Example))
+		} 
+	}
+
+	return buffer.String()
+}
+
+type SearchResults []SearchResult
+
+func (srs SearchResults) String() string {
+	var strBuff bytes.Buffer
+
+	for i := 0; i < len(srs); i++ {
+		strBuff.WriteString(fmt.Sprintf("Definition %d: \n\n", i))
+		strBuff.WriteString(fmt.Sprintf("\t%s", srs[i]))
+	}
+
+	return strBuff.String()
+}
+
+var db *sql.DB
+
+func initDB() (err error) {
+
+	createDefinitionTableSQL := `CREATE TABLE definition (
+		"word" TEXT,
+		"results TEXT
+	);`
+
+	statement, err := db.Prepare(createDefinitionTableSQL)
+	if err != nil {
+		return
+	}
+
+	_, err = statement.Exec()
+
+	return
+
+}
+
+func addResult(word string, res SearchResults) (err error) {
+
+	insertResultSQL := fmt.Sprintf(`INSERT INTO definition ("word", "results") VALUES (%s, %s)`, word, res.String())
+
+	statement, err := db.Prepare(insertResultSQL)
+	if err != nil {
+		return
+	}
+
+	_, err = statement.Exec()
+
+	return
+}
+
+func queryResultsByWord(word string) (sr string, err error) {
+
+	queryByWordSQL := fmt.Sprintf(`SELECT ("results") FROM definition WHERE "word=%s";`, word)
+
+	row, err := db.Query(queryByWordSQL)
+	// defer row.Close()
+	if err != nil {
+		return 
+	}
+
+	if row != nil {
+		row.Scan(&sr)
+	}
+
+	return
+
+}
+
+func init() {
+
+	dirPath := fmt.Sprintf("%s/%s", os.Getenv("HOME"), DIR)
+	dbPath := fmt.Sprintf("%s/%s", dirPath, DB)
+
+	// create dir if uninitialised
+	if _, err := os.Stat(dbPath); err != nil {
+		if os.IsNotExist(err) {
+			// create home dir
+			if err := os.MkdirAll(dirPath, PERMISSIONS); err != nil {
+				fmt.Printf("unable to create internal directories | %s\n", err.Error())
+				os.Exit(1)
+			}
+			// sqlite db
+			if _, err := os.Create(dbPath); err != nil {
+				fmt.Printf("unable to create database | %s\n", err)
+				os.Exit(1)
+			} else {
+				if err := initDB(); err != nil {
+					fmt.Println("unable to initialise database")
+					os.Exit(1)
+				}
+			}
+		} else {
+			fmt.Println("unable to initialise")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	// used to store new results and request old results
+	var err error
+	if db, err = sql.Open("sqlite3", dbPath); err != nil {
+		fmt.Printf("unable to open database | %s\n", err)
+		os.Exit(1)
+	}
+
+
+
+}
+
+func help() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("no arguments required for help command")
+		os.Exit(0)
+	}
+
+	fmt.Printf(`
+	%s: List all commands
+
+	%s: Print all previous searches, --oneline to omit definitions
+
+	%s: Search for definition and record result
+	
+`,
+		HELP, LIST, SEARCH)
+
+}
+
+func list() {
+
+	oneline := false
+	if len(os.Args) > 3 {
+		for i := 0; i < len(os.Args[2:]); i++ {
+			if os.Args[i+2] == ONELINE {
+				oneline = true
+			}
+		}
+	}
+
+	fmt.Println(oneline)
+}
+
+func search() {
+
+	if len(os.Args) < 3 {
+		fmt.Println("requires a single word after the search command")
+		return
+	}
+
+	word := strings.ToLower(os.Args[2])
+	
+	var result string
+	var err error 
+	if result, err = queryResultsByWord(word); err != nil || word == "" {
+
+		fmt.Println(err)
+		fmt.Println(result)
+		fmt.Println("searched!!!")
+
+
+		// // google dictionary api
+		// resp, err := http.Get(fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", word))
+		// if err != nil {
+		// 	fmt.Printf("error occured during search | %s\n", err.Error())
+		// 	os.Exit(1)
+		// }
+
+		// respData := SearchResults{}
+		// if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
+		// 	fmt.Printf("unable to find definition\n")
+		// 	return
+		// }
+
+		// if len(respData) == 0 {
+		// 	return
+		// }
+
+		// result = respData.String()
+
+
+	} 
+
+	fmt.Println(result)
+
+
+}
+
+func main() {
+	defer db.Close()
+
+	if len(os.Args) <= 1 {
+		fmt.Println("use command 'help' for a list of available commands")
+		os.Exit(0)
+	}
+
+	command := strings.ToLower(os.Args[1])
+
+	switch command {
+	case HELP:
+		help()
+	case LIST:
+		fmt.Println("list called")
+	case SEARCH:
+		search()
+	default:
+		fmt.Println("invalid command | use command 'help' for a list of available commands")
+		os.Exit(0)
+	}
+}
